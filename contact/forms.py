68359c145b3891ac6e313d161e8bf1191f7c8578
jorgeromeroc <jromerocontreras@icloud.com>
2023-06-01 00:48:17 -0400

    Estilos bootstrap incorporados en Django

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Nombre y Apellido', max_length=25, min_length=5, required=True)
-    email = forms.CharField(max_length=)
-    message = forms.CharField(max_length=)
+    name = forms.CharField(label='Nombre y Apellido', max_length=25, min_length=5, required=True,
+                           widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'Introduzca los datos'}))
+
+    email = forms.EmailField(label='Correo Electronico', required=True, max_length=100,
+                            widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Correo Electronico'}))
+
+    message = forms.CharField(label='Mensaje', required=True,
+                            widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Mensaje', 'rows': 5}))
